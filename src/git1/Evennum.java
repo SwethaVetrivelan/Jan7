efa3908dcf74e48762c8485520f79aae01e870ad
SwethaVetrivelan <swetha5vsm@gmail.com>
2022-01-07 16:05:10 +0530

    mytask

---
+++ src/git1/Evennum.java
@@ -0,0 +1,13 @@
+package git1;
+
+public class Evennum {
+	public static void main(String[] args) {
+int n=9;
+if(n%2==0) {
+	System.out.println("even");
+}
+else {
+	System.out.println("odd");
+}
+}
+}
